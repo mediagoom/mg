b7a7cf9c11a633058090539dea868050d8599b55
aseduto <aseduto@hotmail.com>
2018-07-17 16:12:55 +0200

    Version and partial ts reading test

---
+++ test/test_core.py
@@ -0,0 +1,44 @@
+import subprocess, os, shutil
+
+class TestError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def getsrcdir():
+	srcdir=os.environ.get('srcdir')
+	if(srcdir == None):
+		srcdir = os.path.dirname(os.path.realpath(__file__))
+	return srcdir
+
+def getroot():
+    cwd = getsrcdir() #os.getcwd()
+    cwd = os.path.abspath(os.path.join(cwd, os.pardir))
+    return cwd
+
+def getmp4():
+	srcdir = getsrcdir()
+	return os.path.join(srcdir, 'test_assets', 'MEDIA1.MP4')
+
+def getmg():
+    cwd = getroot()
+    print cwd
+
+    mg = os.path.join(cwd, 'src', 'mgcli', 'mg')
+
+    print mg
+
+    if not os.path.exists(mg):
+        mg = os.path.join(cwd, 'test', 'out', 'Release', 'mg')
+    if not os.path.exists(mg):
+        mg = os.path.join(cwd, 'test', 'bin', 'Win32', 'Debug', 'mg.exe')
+    if not os.path.exists(mg):
+        mg = os.path.join(cwd, 'test', 'bin', 'Win32', 'Release', 'mg.exe')
+    if not os.path.exists(mg):
+        mg = os.path.join(cwd, 'test', 'bin', 'x64', 'Release', 'mg.exe')
+    
+    if not os.path.exists(mg):
+        raise TestError('mg executable not found')
+
+    return mg
--- test/test_mg.py
+++ test/test_mg.py
@@ -1,53 +1,74 @@
 #!/usr/bin/python
 
 import subprocess, os, shutil
+import hashlib, re
 
-def getsrcdir():
-	srcdir=os.environ.get('srcdir')
-	if(srcdir == None):
-		srcdir = os.path.dirname(os.path.realpath(__file__))
-	return srcdir
+import test_core
 
-def getmp4():
-	srcdir = getsrcdir()
-	return os.path.join(srcdir, 'test_assets', 'MEDIA1.MP4')
+
+def hashres(res):
+	sha1 = hashlib.sha1()
+	sha1.update(res)
+	return sha1.hexdigest()
+
+def doit(cmd, rpl=None, hash=None):
+	testok=False
+	print cmd
+	res = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+	if not rpl is None:
+		for h in rpl:
+			res = re.sub(h, "", res)
+	#print res
+	h =  hashres(res)
+	if not hash is None:
+		if not hash == h:
+			print "invalid hash found " + h + " expected " + hash
+			print ">---------------------------------------------"
+			print res
+			print "<---------------------------------------------"
+		else:
+			print 'hash matched'
+			testok=True
+	else:
+		print "NO HASH: " + h
+		print "//>---------------------------------------------"
+		print res
+		print "//<---------------------------------------------"
+
+	return testok
 
 
 def execmg(mg, mp4):
+	res = []
+	rpl = ['Edit add time \d\d:\d\d:\d\d.\d\d\d']
+	
 	cmd = [mg, '-k:pick', '-i:' + mp4, '-s:0', '-e:20000000']
-	print cmd
-	res = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-	print res
+	res.append(doit(cmd, rpl, '33bb37cd08ca43d101f717c06e0dd880336f1c91'))
+	
 	cmd = [mg, '-k:analyze', '-i:' + mp4]
-	print cmd
-	res = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-	print res
+	res.append(doit(cmd, rpl, 'f196260e03a4c42e1f7fa6fcfcaffdf7b99f09d7'))
 
-cwd = os.getcwd()
-cwd = os.path.abspath(os.path.join(cwd, os.pardir))
-print cwd
+	cmd = [mg, '-k:version']
+	res.append(doit(cmd, None, 'd803c4fffed725bbcca1924baa7b61b660fe8c8e'))
 
-mg = os.path.join(cwd, 'src', 'mgcli', 'mg')
-
-print mg
-
-if not os.path.exists(mg):
-    mg = os.path.join(cwd, 'test', 'out', 'Release', 'mg')
-if not os.path.exists(mg):
-    mg = os.path.join(cwd, 'test', 'bin', 'Win32', 'Debug', 'mg.exe')
-if not os.path.exists(mg):
-    mg = os.path.join(cwd, 'test', 'bin', 'Win32', 'Release', 'mg.exe')
-if not os.path.exists(mg):
-    mg = os.path.join(cwd, 'test', 'bin', 'x64', 'Release', 'mg.exe')
-
-mp4 = getmp4()
+	root = test_core.getroot()
+	ts = os.path.join(root, 'tmp/video_750000_800000.ts')
+	cmd = [mg, '-k:all', '-i:' + ts]
+	res.append(doit(cmd, rpl))
 
 
-    
+	for x in res:
+		if not x:
+			 raise TestError("mg command line failed")
+
+
+mp4 = test_core.getmp4()
+
+mg = test_core.getmg()    
 
 execmg(mg, mp4)
 
-srcd = getsrcdir()
+srcd = test_core.getsrcdir()
 
 #cmd = ['python', os.path.join(srcd, 'test_hash.py'), '--dir', do, '--filter', "'*.*'", '--blueprint', os.path.join(srcd, 'test_assets', 'hls_small.txt')]
 
