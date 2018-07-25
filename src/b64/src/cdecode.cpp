47993cacfd1821ebadd7574fd39ac908f32f6d4b
aseduto <aseduto@hotmail.com>
2018-07-25 11:38:41 +0200

    adding stddef.h reference for using size_t

--- src/b64/src/cdecode.cpp
+++ src/b64/src/cdecode.cpp
@@ -26,7 +26,7 @@ This is part of the libb64 project, and has been placed in the public domain.
 For details, see http://sourceforge.net/projects/libb64
 */
 
-#include <stdint.h>
+#include <stddef.h>
 #include "../include/cdecode.h"
 
 int base64_decode_value(char value_in)
